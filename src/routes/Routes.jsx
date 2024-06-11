d6c551ab781d9557896db004c052906b4ad0dd3a
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-06-11 19:47:51 +0600

    Add 4O4 Page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Login from "../pages/Login/Login";
+import Error from "../pages/Error/Error";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
+        errorElement: <Error />,
         children: [
             {
                 path: '/',
